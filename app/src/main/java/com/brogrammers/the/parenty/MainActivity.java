00c0b970fe184403a0c0a095bcefd0d3c5193406
Sandesh54321 <sandesh54321@gmail.com>
2017-08-22 21:15:54 +0530

    Added components and retrived UI variables in java file

--- app/src/main/java/com/brogrammers/the/parenty/MainActivity.java
+++ app/src/main/java/com/brogrammers/the/parenty/MainActivity.java
@@ -1,14 +1,26 @@
 package com.brogrammers.the.parenty;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_string=(EditText)findViewById(R.id.et_string);
+    EditText et_int=(EditText)findViewById(R.id.et_int);
+    EditText et_longint=(EditText)findViewById(R.id.et_longint);
+    EditText et_date=(EditText)findViewById(R.id.et_date);
+    EditText et_time=(EditText)findViewById(R.id.et_time);
+    Button btn_send=(Button)findViewById(R.id.btn_send);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //some code goes here
+        
+
+
     }
 }
