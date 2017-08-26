c3dfa7b661b66b701d5c28e4a2848e6c3c5a0724
Sandesh54321 <sandesh54321@gmail.com>
2017-08-27 00:22:58 +0530

    Created a demo activity to transfer 5 datatypes to server Added a databaseapi class for all database operations using volley library.

---
+++ app/src/main/java/com/brogrammers/the/parenty/DbAPICall.java
@@ -0,0 +1,29 @@
+package com.brogrammers.the.parenty;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.Map;
+
+/**
+ * Created by Sandesh on 22-08-2017.
+ */
+
+public class DbAPICall extends StringRequest {
+
+    private static String REGISTER_REQUEST_URL="https://3musketers2017.000webhostapp.com/parenty/";
+    private Map params;
+
+
+
+    public DbAPICall(Map params, String phpapi, Response.Listener<String> listener)
+    {
+        super(Method.POST,REGISTER_REQUEST_URL+phpapi,listener,null);
+        this.params=params;
+    }
+
+    @Override
+    public Map getParams() {
+        return params;
+    }
+}
